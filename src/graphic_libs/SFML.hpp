06355528126d0b33cea9f9654c3cfbce2518ed95
Jocelyn-JE <jocelyn.jean-elie@epitech.eu>
2025-03-28 17:41:54 +0100

    ✨ Add SFML graphics library implementation according to IDisplayModule interface

---
+++ src/graphic_libs/SFML.hpp
@@ -0,0 +1,37 @@
+/*
+** EPITECH PROJECT, 2025
+** bsarcade
+** File description:
+** bar
+*/
+
+#ifndef SRC_GRAPHIC_LIBS_SFML_HPP_
+    #define SRC_GRAPHIC_LIBS_SFML_HPP_
+
+    #include <string>
+    #include <memory>
+    #include <map>
+    #include "modules/IDisplayModule.hpp"
+
+namespace libs {
+namespace graphic {
+class SFML : public IDisplayModule {
+ public:
+    static std::map<sf::Keyboard::Key, Key::KeyCode> keys;
+    static std::map<sf::Mouse::Button, Key::KeyCode> mouse_buttons;
+    SFML();
+    void createWindow(const Window &window) override;
+    void draw(const IDrawable &to_draw) override;
+    void display(void) override;
+    void clear(void) override;
+    Event getEvent(void) override;
+    void handleSound(const Sound &sound) override;
+    ~SFML();
+ private:
+    std::unique_ptr<sf::RenderWindow> _window;
+    std::map<std::string, sf::Sound> _sounds;
+};
+}  // namespace graphic
+}  // namespace libs
+
+#endif  // SRC_GRAPHIC_LIBS_SFML_HPP_
