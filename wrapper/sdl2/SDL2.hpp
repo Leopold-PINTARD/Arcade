cf0172515d521bd6a3c82893c0baf91346437c35
mathys-f <mathys.fillion@epitech.eu>
2025-04-02 15:49:55 +0200

    ✨ Add SDL2 wrapper class with window management and rendering methods

---
+++ wrapper/sdl2/SDL2.hpp
@@ -0,0 +1,45 @@
+/*
+** EPITECH PROJECT, 2025
+** SDL2.hpp
+** File description:
+** SDL2.hpp
+*/
+
+#ifndef WRAPPER_SDL2_SDL2_HPP_
+    #define WRAPPER_SDL2_SDL2_HPP_
+
+    #include <SDL2/SDL.h>
+    #include <SDL2/SDL_ttf.h>
+    #include <SDL2/SDL_image.h>
+    #include <string>
+
+class SDL2 {
+ public:
+    SDL2();
+    ~SDL2();
+
+    void createWindow(const std::string &title,
+        const std::string &iconPath, int width, int height);
+    void clearWindow(void);
+    void displayWindow(void);
+
+    bool isRunning(void);
+
+    void pollEvent(void);
+
+    TTF_Font *loadFont(const std::string &file, int size);
+    void drawText(TTF_Font *font, const std::string text,
+        SDL_Color color, int x, int y);
+    void drawSprite(const std::string &file, int x, int y);
+
+    void getSDLError(void);
+    void getTTFError(void);
+
+ private:
+    SDL_Window *window;
+    SDL_Renderer *renderer;
+    SDL_Event event;
+    bool running;
+};
+
+#endif  // WRAPPER_SDL2_SDL2_HPP_
