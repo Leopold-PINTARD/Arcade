1f92b8347d67ab044457d083ff9ed444ef09f77a
mathys-f <mathys.fillion@epitech.eu>
2025-04-06 15:00:52 +0200

    ✨ Add SDL2_DL header for SDL2 graphics handling

---
+++ src/graphic_libs/SDL2.hpp
@@ -0,0 +1,38 @@
+/*
+** EPITECH PROJECT, 2025
+** SDL2.hpp
+** File description:
+** SDL2.hpp
+*/
+
+#ifndef SRC_GRAPHIC_LIBS_SDL2_HPP_
+#define SRC_GRAPHIC_LIBS_SDL2_HPP_
+
+#include <map>
+
+#include "modules/IDisplayModule.hpp"
+#include "wrapper/sdl2/SDL2.hpp"
+
+namespace libs {
+namespace graphic {
+
+class SDL2_DL : public IDisplayModule {
+ public:
+    SDL2_DL();
+    ~SDL2_DL();
+    void createWindow(const Window &window) override;
+    void draw(const IDrawable &to_draw) override;
+    void display(void) override;
+    void clear(void) override;
+    Event getEvent(void) override;
+    void handleSound(const Sound &sound) override;
+    static std::map<SDL_Keycode, Key::KeyCode> keys;
+
+ private:
+    SDL2 sdl2;
+};
+
+}  // namespace graphic
+}  // namespace libs
+
+#endif  // SRC_GRAPHIC_LIBS_SDL2_HPP_
