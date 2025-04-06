ea12b929b166bff574fb0950404dd587d471c5cc
mathys-f <mathys.fillion@epitech.eu>
2025-04-06 14:57:37 +0200

    ✨ Add SDL2 library loading and unloading functions

---
+++ src/graphic_libs/SDL2.cpp
@@ -0,0 +1,24 @@
+/*
+** EPITECH PROJECT, 2025
+** SDL2.cpp
+** File description:
+** SDL2.cpp
+*/
+
+#include <memory>
+#include <iostream>
+#include <map>
+#include <utility>
+
+#include "src/graphic_libs/SDL2.hpp"
+#include "src/log/Log.hpp"
+#include "DataStructures/Sprite.hpp"
+#include "DataStructures/Text.hpp"
+
+__attribute__((constructor)) void load(void) {
+    Log::info() << "Loading SDL2 lib..." << std::endl;
+}
+
+__attribute__((destructor)) void unload(void) {
+    Log::info() << "Unloading SDL2 lib..." << std::endl;
+}
