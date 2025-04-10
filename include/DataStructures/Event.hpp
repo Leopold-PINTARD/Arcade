c11daeaefa120c205cd08bed3fcedcb88ba0556d
Jocelyn-JE <jocelyn.jean-elie@epitech.eu>
2025-04-10 15:00:50 +0200

    🔇 Remove all debug in Event.hpp

--- include/DataStructures/Event.hpp
+++ include/DataStructures/Event.hpp
@@ -31,42 +31,45 @@ class Event {
         float value;
     };
 
-    Event(Key::KeyCode keyCode, std::any data) : key(keyCode), value(data) {
-        std::cout << "Event created with key: " << keyCode
-                  << ", data type: " << data.type().name() << std::endl;
+    Event(enum Key::KeyCode keyCode, std::any data)
+        : key(keyCode), value(data) {
+        // std::cout << "Event created with key: " << keyCode
+        //           << ", data type: " << data.type().name() << std::endl;
     }
-    ~Event() { std::cout << "Event destroyed with key: " << key << std::endl; }
-    Event(const Event& other) : key(other.key), value(other.value) {
-        std::cout << "Event copied with key: " << other.key << std::endl;
+    ~Event() {
+        // std::cout << "Event destroyed with key: " << key << std::endl;
     }
-    Event& operator=(const Event& other) {
-        std::cout << "Copying event with key: " << other.key << std::endl;
-        if (this != &other) {
-            std::cout << "1" << std::endl;
-            key = other.key;
-            std::cout << "2" << std::endl;
-            if (other.value.type() == typeid(MousePos)) {
-                value = std::any_cast<MousePos>(other.value);
-                std::cout << "2 pos" << std::endl;
-            } else if (other.value.type() == typeid(MouseStatusClick)) {
-                value = std::any_cast<MouseStatusClick>(other.value);
-                std::cout << "2 lcick" << std::endl;
-            } else if (other.value.type() == typeid(MouseStatusScroll)) {
-                value = std::any_cast<MouseStatusScroll>(other.value);
-                std::cout << "2 scroll" << std::endl;
-            } else if (other.value.type() == typeid(int)) {
-                value = std::any_cast<int>(other.value);
-                std::cout << "2 int" << std::endl;
-            } else if (other.value.type() == typeid(KeyStatus)) {
-                value = std::any_cast<KeyStatus>(other.value);
-                std::cout << "2 status" << std::endl;
-            }
-            std::cout << "3" << std::endl;
-        }
-        std::cout << "Event assigned with key: " << other.key << std::endl;
-        return *this;
-    }
+    // Event(const Event& other) : key(other.key), value(other.value) {
+    //     std::cout << "Event copied with key: " << other.key << std::endl;
+    // }
+    // Event& operator=(const Event& other) {
+    //     std::cout << "Copying event with key: " << other.key << std::endl;
+    //     if (this != &other) {
+    //         std::cout << "1" << std::endl;
+    //         key = other.key;
+    //         std::cout << "2" << std::endl;
+    //         if (other.value.type() == typeid(MousePos)) {
+    //             value = std::any_cast<MousePos>(other.value);
+    //             std::cout << "2 pos" << std::endl;
+    //         } else if (other.value.type() == typeid(MouseStatusClick)) {
+    //             value = std::any_cast<MouseStatusClick>(other.value);
+    //             std::cout << "2 lcick" << std::endl;
+    //         } else if (other.value.type() == typeid(MouseStatusScroll)) {
+    //             value = std::any_cast<MouseStatusScroll>(other.value);
+    //             std::cout << "2 scroll" << std::endl;
+    //         } else if (other.value.type() == typeid(int)) {
+    //             value = std::any_cast<int>(other.value);
+    //             std::cout << "2 int" << std::endl;
+    //         } else if (other.value.type() == typeid(KeyStatus)) {
+    //             value = std::any_cast<KeyStatus>(other.value);
+    //             std::cout << "2 status" << std::endl;
+    //         }
+    //         std::cout << "3" << std::endl;
+    //     }
+    //     std::cout << "Event assigned with key: " << other.key << std::endl;
+    //     return *this;
+    // }
 
-    enum Key::KeyCode key;
-    std::any value;
+    const enum Key::KeyCode key;
+    const std::any value;
 };
