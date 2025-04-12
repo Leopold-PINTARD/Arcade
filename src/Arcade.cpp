b93766183782f463959c398555068749738b3755
Jocelyn-JE <jocelyn.jean-elie@epitech.eu>
2025-04-12 23:18:41 +0200

    ✨ Add basic menu with lib selection

--- src/Arcade.cpp
+++ src/Arcade.cpp
@@ -7,44 +7,109 @@
 
 #include "Arcade.hpp"
 
+#include <algorithm>
 #include <chrono>
+#include <filesystem>
 #include <iostream>
 #include <memory>
 #include <string>
 
+#include <DataStructures/Text.hpp>
+
 #include "./log/Log.hpp"
 
 Arcade::Arcade(std::string gfxLib)
-    : _gfxLoader(gfxLib),
-      _gameLoader("./lib/arcade_minesweeper.so"),
+    : _previousTime(std::chrono::high_resolution_clock::now()),
+      _currentTime(std::chrono::high_resolution_clock::now()),
+      _inMenu(true),
+      _gfxLoader(gfxLib),
+      _gameLoader(),
       _displayModule(_gfxLoader.getInstance("getDisplayModule")),
-      _gameModule(_gameLoader.getInstance("getGameModule")) {}
+      _gameModule(_gameLoader.getInstance("getGameModule")),
+      _currentGfxLib(gfxLib),
+      _currentGameLib("") {}
 
 Arcade::~Arcade() {}
 
-bool Arcade::handleEvent() {
+void Arcade::cycleCurrentGfxLib() {
+    auto it = std::find(_gfxModules.begin(), _gfxModules.end(), _currentGfxLib);
+    if (it != _gfxModules.end() && ++it != _gfxModules.end())
+        _currentGfxLib = *it;
+    else
+        _currentGfxLib = _gfxModules.front();
+}
+
+void Arcade::cycleCurrentGameLib() {
+    auto it =
+        std::find(_gameModules.begin(), _gameModules.end(), _currentGameLib);
+    if (it != _gameModules.end() && ++it != _gameModules.end())
+        _currentGameLib = *it;
+    else
+        _currentGameLib = _gameModules.front();
+}
+
+bool Arcade::handleMenuEvent() {
+    Event currentEvent = _displayModule->getEvent();
+
+    if (currentEvent.key == Key::KeyCode::NONE) return false;
+    if (currentEvent.key == Key::KeyCode::ECHAP) std::exit(0);
+    if (currentEvent.key == Key::KeyCode::KEY_1) {
+        try {
+            if (std::any_cast<Event::KeyStatus>(currentEvent.value) !=
+                Event::KeyStatus::KEY_PRESSED)
+                return true;
+        } catch (const std::bad_any_cast &e) {
+            return true;
+        }
+        cycleCurrentGfxLib();
+    }
+    if (currentEvent.key == Key::KeyCode::KEY_2) {
+        try {
+            if (std::any_cast<Event::KeyStatus>(currentEvent.value) !=
+                Event::KeyStatus::KEY_PRESSED)
+                return true;
+        } catch (const std::bad_any_cast &e) {
+            return true;
+        }
+        cycleCurrentGameLib();
+    }
+    return true;
+}
+
+bool Arcade::handleGameEvent() {
     Event currentEvent = _displayModule->getEvent();
 
     _gameModule->event(currentEvent);
     if (currentEvent.key == Key::KeyCode::NONE) return false;
-    if (currentEvent.key == Key::KeyCode::SUPPR) std::exit(0);
+    if (currentEvent.key == Key::KeyCode::ECHAP) std::exit(0);
     if (currentEvent.key == Key::KeyCode::KEY_1) {
         try {
-            std::any_cast<Event::KeyStatus>(currentEvent.value);
+            if (std::any_cast<Event::KeyStatus>(currentEvent.value) !=
+                Event::KeyStatus::KEY_PRESSED)
+                return true;
         } catch (const std::bad_any_cast &e) {
             return true;
         }
         currentEvent.~Event();
-        _gfxLoader.switchLib("./lib/arcade_sfml.so");
+        cycleCurrentGfxLib();
+        _gfxLoader.switchLib(_currentGfxLib);
         if (_gfxLoader.getInstance("getDisplayModule") == nullptr)
             std::exit(84);
         if (_displayModule == nullptr) std::exit(84);
         _displayModule->createWindow(_gameModule->getWindow());
     }
-    if (currentEvent.key == Key::KeyCode::KEY_2) {
+    if (currentEvent.key == Key::KeyCode::KEY_2 ||
+        currentEvent.key == Key::KeyCode::KEY_3) {
+        try {
+            if (std::any_cast<Event::KeyStatus>(currentEvent.value) !=
+                Event::KeyStatus::KEY_PRESSED)
+                return true;
+        } catch (const std::bad_any_cast &e) {
+            return true;
+        }
         currentEvent.~Event();
-        std::cout << "Switching to pacman" << std::endl;
-        _gameLoader.switchLib("./lib/arcade_pacman.so");
+        if (currentEvent.key == Key::KeyCode::KEY_2) cycleCurrentGameLib();
+        _gameLoader.switchLib(_currentGameLib);
         if (_gameLoader.getInstance("getGameModule") == nullptr) std::exit(84);
         if (_gameModule == nullptr) std::exit(84);
         _displayModule->createWindow(_gameModule->getWindow());
@@ -53,25 +118,117 @@ bool Arcade::handleEvent() {
 }
 
 void Arcade::run() {
-    auto previousTime = std::chrono::high_resolution_clock::now();
-    auto currentTime = std::chrono::high_resolution_clock::now();
-
+    updateLibLists("./lib/");
     Log::setDebug(false);
-    _displayModule->createWindow(_gameModule->getWindow());
+    if (!_inMenu)
+        _displayModule->createWindow(_gameModule->getWindow());
+    else
+        _displayModule->createWindow({{32, 32}, "Arcade", "./assets/icon.png"});
     while (true) {
-        currentTime = std::chrono::high_resolution_clock::now();
-        std::chrono::duration<float> elapsedTime = currentTime - previousTime;
-        previousTime = currentTime;
-
-        _displayModule->clear();
-        while (handleEvent()) {
-        }
-        for (const std::unique_ptr<IDrawable> &drawable :
-             _gameModule->getDrawables())
-            _displayModule->draw(*drawable);
-        for (Sound sound : _gameModule->getSound())
-            _displayModule->handleSound(sound);
-        _displayModule->display();
-        _gameModule->update(elapsedTime.count());
+        if (_inMenu)
+            menuLoop();
+        else
+            gameLoop();
     }
+}
+
+void Arcade::gameLoop() {
+    _currentTime = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<float> elapsedTime = _currentTime - _previousTime;
+    _previousTime = _currentTime;
+
+    _displayModule->clear();
+    while (handleGameEvent()) {
+    }
+    for (const std::unique_ptr<IDrawable> &drawable :
+         _gameModule->getDrawables())
+        _displayModule->draw(*drawable);
+    for (Sound sound : _gameModule->getSound())
+        _displayModule->handleSound(sound);
+    _displayModule->display();
+    _gameModule->update(elapsedTime.count());
+}
+
+void Arcade::menuLoop() {
+    _currentTime = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<float> elapsedTime = _currentTime - _previousTime;
+    _previousTime = _currentTime;
+
+    _displayModule->clear();
+    while (handleMenuEvent()) {
+    }
+    int i = 0;
+    Text infoText;
+    infoText.setStr("Graphic Libraries");
+    infoText.setGUI_Color({255, 255, 255, 255});
+    infoText.setPosition({0, 0});
+    infoText.setFontPath("./assets/fonts/NotoSans.ttf");
+    infoText.setScale({10, 10});
+    _displayModule->draw(infoText);
+    for (auto lib : _gfxModules) {
+        Text text;
+        if (lib == _currentGfxLib)
+            text.setStr("> " + lib);
+        else
+            text.setStr("- " + lib);
+        text.setGUI_Color({255, 255, 255, 255});
+        text.setPosition({0, i + 1});
+        text.setFontPath("./assets/fonts/NotoSans.ttf");
+        text.setScale({10, 10});
+        _displayModule->draw(text);
+        i++;
+    }
+    infoText.setStr("Game Libraries");
+    infoText.setPosition({0, i + 2});
+    _displayModule->draw(infoText);
+    for (auto lib : _gameModules) {
+        Text text;
+        if (lib == _currentGameLib)
+            text.setStr("> " + lib);
+        else
+            text.setStr("- " + lib);
+        text.setGUI_Color({255, 255, 255, 255});
+        text.setPosition({0, i + 3});
+        text.setFontPath("./assets/fonts/NotoSans.ttf");
+        text.setScale({10, 10});
+        _displayModule->draw(text);
+        i++;
+    }
+    _displayModule->display();
+}
+
+void Arcade::updateLibLists(std::string path) {
+    std::filesystem::path dirPath(path);
+
+    if (!std::filesystem::exists(dirPath) ||
+        !std::filesystem::is_directory(dirPath)) {
+        Log::error() << "Error opening directory: " << path << std::endl;
+        return;
+    }
+
+    for (const auto &entry : std::filesystem::directory_iterator(dirPath)) {
+        if (entry.is_regular_file() && entry.path().extension() == ".so") {
+            try {
+                DLLoader<IDisplayModule> gfxLoader(
+                    entry.path().relative_path().string());
+                if (gfxLoader.entrypointExists("getDisplayModule") == true) {
+                    _gfxModules.push_back(
+                        entry.path().relative_path().string());
+                    continue;
+                }
+            } catch (const DLLoader<IDisplayModule>::DLLoaderException &e) {
+                std::cout << "Not gfx: " << e.what() << std::endl;
+            }
+            try {
+                DLLoader<IGameModule> gameLoader(
+                    entry.path().relative_path().string());
+                if (gameLoader.entrypointExists("getGameModule") == false)
+                    continue;
+                _gameModules.push_back(entry.path().relative_path().string());
+            } catch (const DLLoader<IGameModule>::DLLoaderException &e) {
+                std::cout << "Not game: " << e.what() << std::endl;
+            }
+        }
+    }
+    _currentGameLib = _gameModules.front();
 }
--- src/Arcade.hpp
+++ src/Arcade.hpp
@@ -8,8 +8,10 @@
 #ifndef SRC_ARCADE_HPP_
 #define SRC_ARCADE_HPP_
 
+#include <chrono>
 #include <memory>
 #include <string>
+#include <vector>
 
 #include <modules/IDisplayModule.hpp>
 #include <modules/IGameModule.hpp>
@@ -30,11 +32,24 @@ class Arcade {
 
  protected:
  private:
-    bool handleEvent();
+    bool handleMenuEvent();
+    bool handleGameEvent();
+    void updateLibLists(std::string path);
+    void menuLoop();
+    void gameLoop();
+    void cycleCurrentGfxLib();
+    void cycleCurrentGameLib();
+    std::chrono::_V2::system_clock::time_point _previousTime;
+    std::chrono::_V2::system_clock::time_point _currentTime;
+    bool _inMenu;
     DLLoader<IDisplayModule> _gfxLoader;
     DLLoader<IGameModule> _gameLoader;
     std::unique_ptr<IDisplayModule> &_displayModule;
     std::unique_ptr<IGameModule> &_gameModule;
+    std::string _currentGfxLib;
+    std::string _currentGameLib;
+    std::vector<std::string> _gameModules;
+    std::vector<std::string> _gfxModules;
 };
 
 #endif  // SRC_ARCADE_HPP_
