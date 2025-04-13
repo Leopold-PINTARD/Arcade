cbc5a3567f6cc693c1bd8d82406fb900508acebf
mathys-f <mathys.fillion@epitech.eu>
2025-04-13 06:29:39 +0200

    ✨ Add initDrawablesMap method to initialize drawable sprites in Snake game

--- src/game_libs/SnakeInit.cpp
+++ src/game_libs/SnakeInit.cpp
@@ -20,3 +20,14 @@ static Sprite createSprite(const std::string path, const std::string text) {
     sprite.setCLI_Textures({text});
     return sprite;
 }
+
+void libs::game::Snake::initDrawablesMap(void) {
+    drawablesMap["snake"] = std::make_unique<Sprite>(
+        createSprite("assets/snake/body_horizontal.png", "o "));
+    drawablesMap["apple"] = std::make_unique<Sprite>(
+        createSprite("assets/snake/apple.png", "a "));
+    drawablesMap["light_green"] = std::make_unique<Sprite>(
+        createSprite("assets/snake/light_green.png", "# "));
+    drawablesMap["dark_green"] = std::make_unique<Sprite>(
+        createSprite("assets/snake/dark_green.png", "# "));
+}
